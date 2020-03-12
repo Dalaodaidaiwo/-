42b9a48922d03d48cffdf2aaf146c8f17dc0b006
Mowenyii <50766241+Mowenyii@users.noreply.github.com>
2020-03-12 22:26:10 +0800

    Update index.js

--- scrm-master/miniprogram/pages/index/index.js
+++ scrm-master/miniprogram/pages/index/index.js
@@ -15,25 +15,50 @@ Page({
     let that=this;
     const db=wx.cloud.database();
     const _=db.command;
-
+    
     app.callbacks.push((userInfo)=>{
+      //console.log("hi")
+      //console.log(userInfo)
       this.setData({
         userInfo:userInfo
       });
     });
 
     app.callbacks.push((userInfo)=>{
+      
+      db.collection('statistics-first-page').add({
+        // data 字段表示需新增的 JSON 数据
+        data: {
+          // _id: 'todo-identifiant-aleatoire', // 可选自定义 _id，在此处场景下用数据库自动分配的就可以了
+          customers: "1790",
+          msg_today: "666",
+          visitors_today: "55",
+          visitors_total: "886",
+          userId:"1583630225707_0.8837796301953007_33609536"
+        },
+        success: function(res) {
+          // res 是一个对象，其中有 _id 字段标记刚创建的记录的 id
+          console.log(res)
+        }
+      });
+      
       db.collection("statistics-first-page")
         .where({
-          userId:userInfo._id
+          _openid:userInfo._openid
         })
+      
         .limit(1)
         .get({
+          
           success:function(res){
+            //console.log(userId)
+            //console.log(userInfo._openid);
+            //console.log("diu");
+            //console.log(res);
             if(res&&res.data&&res.data.length){
               that.setData({
                 statData:res.data[0]
-              });
+              });       
             }
           },
           fail:function(event){
