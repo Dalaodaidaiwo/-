4614ce41d7a8fa277229604932dfc9211b3c82f0
Mowenyii <50766241+Mowenyii@users.noreply.github.com>
2020-05-04 21:26:12 +0800

    Create index.js

---
+++ scrm-master/cloudfunctions/getNum/index.js
@@ -0,0 +1,18 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+cloud.init({
+  // API 调用都保持和云函数当前所在环境一致
+  env: cloud.DYNAMIC_CURRENT_ENV
+})
+const db = cloud.database().collection('num')
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  let data = []
+
+  await db.get().then((res) => {
+    data = res
+  })
+  console.log(data)
+  return data
+}
